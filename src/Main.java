04bcd2ccc0ec7620a5e94976c32f83295dd8e80b
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2023-03-10 15:13:28 +0100

    es1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import org.lessons.java.shop.Product;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Product product1 = new Product("orologio", 400, "un oggetto che indica l'ora corrente");
+
+        product1.printDetailsProduct();
+
+        Product product2 = new Product();
+
+        product2.setName("braccialetto");
+        product2.setDescription("un oggetto che si indossa al polso");
+        product2.setBasePrice(45.50);
+
+        product2.printDetailsProduct();
+
+
     }
 }
---
+++ src/org/lessons/java/shop/Product.java
@@ -0,0 +1,92 @@
+package org.lessons.java.shop;
+
+import java.text.DecimalFormat;
+import java.util.Random;
+
+public class Product {
+    private int code;
+    private String name;
+    private String description;
+    private double basePrice;
+    private double fullPrice;
+    private final static double IVA = 0.22;
+
+
+    public Product(String name, double price ,String description) {
+        Random random = new Random();
+        this.code = random.nextInt(Integer.MAX_VALUE);
+        this.name = name;
+        this.basePrice = price;
+        this.description = description;
+        this.setFullPrice(this.basePrice);
+    }
+
+    public Product() {
+        Random random = new Random();
+        this.code = random.nextInt(1000000);
+        this.name = "";
+        this.basePrice = 0;
+        this.description = "";
+        this.setFullPrice(this.basePrice);
+    }
+
+    public static double getIVA(){
+        return IVA;
+    }
+    public int getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+        this.setFullPrice(this.basePrice);
+    }
+
+    public double getFullPrice() {
+        return fullPrice;
+    }
+    private void setFullPrice(double basePrice){
+        this.fullPrice = basePrice + basePrice * IVA;
+    }
+    public String getFullName(){
+        return this.code + this.name;
+    }
+    public String getFormattedCode(){
+        String pattern = "00000000";
+        DecimalFormat decimalFormat = new DecimalFormat(pattern);
+        String format = decimalFormat.format(this.code);
+        return  format;
+    }
+
+    public void printDetailsProduct(){
+        System.out.println("\nEcco il tuo oggetto\n");
+        System.out.println("IVA: " + this.getIVA());
+        System.out.println("Codice: " + this.getCode());
+        System.out.println("Nome: " + this.getName());
+        System.out.println("Descrizione: " + this.getDescription());
+        System.out.println("Prezzo base: " + this.getBasePrice());
+        System.out.println("Prezzo completo: " + this.getFullPrice());
+        System.out.println("Nome completo: " + this.getFullName());
+        System.out.println("Codice formattato: " + this.getFormattedCode());
+    }
+}
